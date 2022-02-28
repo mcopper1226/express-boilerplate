ab5fa2d9886e94acc9f040d7bb0a7f6de2cd4e08
Copper <megan.copper@disney.com>
2022-02-28 09:05:36 -0500

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,8 @@ var _bodyParser = require("body-parser");
 
 var _cors = _interopRequireDefault(require("cors"));
 
+var _path = _interopRequireDefault(require("path"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const express = require('express');
@@ -18,9 +20,17 @@ app.use((0, _cors.default)());
 app.use((0, _bodyParser.json)());
 app.use((0, _bodyParser.urlencoded)({
   extended: true
-}));
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+})); // Have Node serve the files for our built React app
+
+app.use(express.static(_path.default.resolve(__dirname, '../client/build'))); // Handle GET requests to /api route
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello from server!'
+  });
+});
+app.get('*', (req, res) => {
+  res.sendFile(_path.default.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 const start = async () => {
--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
-import { json, urlencoded } from 'body-parser'
-import cors from 'cors'
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
+import path from 'path';
 
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 const start = async () => {
   try {
     app.listen(port, () => {
-      console.log(`REST API on http://localhost:${port}`)
-    })
+      console.log(`REST API on http://localhost:${port}`);
+    });
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
-}
+};
 
-start()
+start();
